2785ed98fe0cb501aaa3724b994baaa94cabc435
vinitha <vs.vinitha@gmail.com>
2013-09-06 11:12:44 +0530

    commit0906201305
    commit0906201305

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 Handlebars.registerHelper("debug", function(optionalValue) {
   console.log("Current Context");
   console.log("====================");
@@ -9,6 +27,10 @@ Handlebars.registerHelper("debug", function(optionalValue) {
     console.log(optionalValue);
   }
 });
+
+
+
+
 
 var app = {
     // Application Constructor
@@ -41,19 +63,20 @@ var app = {
         console.log('Received Event: ' + id);
     },
 
-    blogHome: function(){
-   
+
+    blog: function(){
+        function getBlogs() {
             var dfd = $.Deferred();
-				$.support.cors = true;
-  $.mobile.allowCrossDomainPages = true;
             $.ajax({
-                url: 'http://alexbachuk.com/api/get_recent_posts/',
+                url: 'http://dynmsales.com/api/get_page/?id=133',
                 type: 'GET',
                 dataType: 'json',
                 success: function(data){
-                                  
-                    $('#home-content').html("helloooooooooooo");
-                   
+                    var source   = $("#blog-template").html();
+                    var template = Handlebars.compile(source);
+                    var blogData = template(data);
+                    $('#blog-data').html(blogData);
+                    $('#blog-data').trigger('create');
                     dfd.resolve(data);
 
                 },
@@ -62,8 +85,13 @@ var app = {
                 }
             });
             return dfd.promise();
-       
+        };
 
+        getBlogs().then(function(data){
+            $('#all-posts').on('click','li', function(e){                
+                localStorage.setItem('postData', JSON.stringify(data.posts[$(this).index()]));
+            });
+        });
 
         
     },
